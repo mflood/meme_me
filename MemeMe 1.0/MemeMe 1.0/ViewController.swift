8a563008c134258750da3305f63acf47c67526af
mflood <hermannwest@gmail.com>
2023-02-12 17:57:24 -0700

    Setup textfield fonts

--- MemeMe 1.0/MemeMe 1.0/ViewController.swift
+++ MemeMe 1.0/MemeMe 1.0/ViewController.swift
@@ -19,10 +19,22 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UITextF
     
     func setupTextField(textfield: UITextField, initialText: String)
     {
-        textfield.text = initialText
-        textfield.textAlignment = .center
         textfield.delegate = self
-        
+                
+        let memeTextAttributes: [NSAttributedString.Key: Any] = [
+                    NSAttributedString.Key.strokeColor: UIColor.black,
+                    NSAttributedString.Key.foregroundColor: UIColor.white,
+                    NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+                    NSAttributedString.Key.strokeWidth:  4.0
+                ]
+                
+        textfield.defaultTextAttributes = memeTextAttributes
+                
+        // This stuff needs to happen after defaultTextAttributes are set!
+        // or they get reset to defaults...
+        textfield.textAlignment = .center
+            
+        textfield.text = initialText
     }
     
     override func viewDidLoad() {
