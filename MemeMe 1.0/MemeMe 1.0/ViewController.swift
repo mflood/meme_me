67f68a0552c388ea44f51306eb5f74dd37b7f736
mflood <hermannwest@gmail.com>
2023-02-12 17:17:46 -0700

    Add and hookup IBOutlets

--- MemeMe 1.0/MemeMe 1.0/ViewController.swift
+++ MemeMe 1.0/MemeMe 1.0/ViewController.swift
@@ -10,23 +10,35 @@ import UIKit
 class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
 
     @IBOutlet var cameraButton: UIBarButtonItem? = nil
-    @IBOutlet var image: UIImage? = nil
+    @IBOutlet var cancelButton: UIBarButtonItem? = nil
+    @IBOutlet var actionButton: UIBarButtonItem? = nil
+    @IBOutlet var image: UIImageView? = nil
+    @IBOutlet var topText: UITextField? = nil
+    @IBOutlet var bottomText: UITextField? = nil
     // @IBOutlet var actionButton:
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.actionButton?.isEnabled = false
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.cameraButton?.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+        
     }
 
     @IBAction func presentImagePicker(_ sender: Any) {
         let imagePicker = UIImagePickerController()
         imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
         present(imagePicker, animated: true)
     }
     
     @IBAction func presentCamera(_ sender: Any) {
-        if(UIImagePickerController .isSourceTypeAvailable(UIImagePickerController.SourceType.camera))
+        if(UIImagePickerController .isSourceTypeAvailable(.camera))
         {
             let imagePicker = UIImagePickerController()
             imagePicker.sourceType = UIImagePickerController.SourceType.camera
@@ -54,15 +66,30 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavi
         self.present(controller, animated: true, completion: nil)
     }
     
+    @IBAction func handleCancel(_ sender: Any) {
+        print("cancelled")
+        self.resetView()
+    }
+    
+    func resetView() {
+        self.topText?.text = ""
+        self.bottomText?.text = ""
+        self.image?.image = nil
+    }
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        print("Finished picking")
+        
         picker.dismiss(animated: true)
         
+        self.actionButton?.isEnabled = true
         
     }
         
   
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         picker.dismiss(animated: true)
+        self.actionButton?.isEnabled = false
     }
     
 
