9fd4cc88b497cd2d7af56fcaf6c98ce9c7200fc5
mflood <hermannwest@gmail.com>
2023-02-21 10:01:03 -0700

    Rename view controller

--- MemeMe 1.0/MemeMe 1.0/ViewController.swift
+++ MemeMe 1.0/MemeMe 1.0/ViewController.swift
@@ -11,7 +11,7 @@ let debug: Bool = false
 
 
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UITextFieldDelegate, UINavigationControllerDelegate {
+class EditMemeViewController: UIViewController, UIImagePickerControllerDelegate, UITextFieldDelegate, UINavigationControllerDelegate {
 
     @IBOutlet var cameraButton: UIBarButtonItem? = nil
     @IBOutlet var cancelButton: UIBarButtonItem? = nil
