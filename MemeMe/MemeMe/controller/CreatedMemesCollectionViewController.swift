dbbbd9df1248e711bf9886f1ae9cf40195f9fb07
mflood <hermannwest@gmail.com>
2023-02-22 10:59:00 -0700

    Add + button to collection controller

--- MemeMe/MemeMe/controller/CreatedMemesCollectionViewController.swift
+++ MemeMe/MemeMe/controller/CreatedMemesCollectionViewController.swift
@@ -14,7 +14,25 @@ class CreatedMemesCollectionViewController: UICollectionViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(presentEditMeme))
     }
+    
+    
+    @objc func presentEditMeme() {
+            debugPrint("clicked")
+        
+        // Grab the DetailVC from Storyboard
+     let editMemeViewController = self.storyboard!.instantiateViewController(withIdentifier: "EditMemeViewController") as! EditMemeViewController
+
+        //Populate view controller with data from the selected item
+       // detailController.villain = allVillains[(indexPath as NSIndexPath).row]
+
+        // Present the view controller using navigation
+        navigationController!.pushViewController(editMemeViewController, animated: true)
+        
+    }
+    
     
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
